e4b221b0522ff975c4c135be3de5d74b8060a7b7
Shama Parveen <139287494+shamap01@users.noreply.github.com>
2024-12-27 06:02:02 +0530

    Create TodoList.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { TodoContext } from '../context/TodoContext';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+    const { todos } = useContext(TodoContext);
+
+    return (
+        <ul>
+            {todos.map(todo => (
+                <TodoItem key={todo.id} todo={todo} />
+            ))}
+        </ul>
+    );
+};
+
+export default TodoList;
